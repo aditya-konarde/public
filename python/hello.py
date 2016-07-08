fe5910a6e1cc792b2f6b0332759e5bb2ad5bda6b
Aditya Konarde <aditya.konarde@gmail.com>
2016-07-08 21:09:03 +0530

    First demo for python. Covers strings and arithmetic

--- python/hello.py
+++ python/hello.py
@@ -1,2 +1,41 @@
-print "hello, world!"
-print "Just making some random changes"
+import random
+import sys
+import os
+
+print("Hello world")
+
+#comment
+
+'''
+Multiline
+comment
+'''
+
+#Strings
+name = "Aditya"
+print(name)
+
+#Arithmetic
+print("5 + 2 =", 5+2)
+print("5 - 2 =", 5-2)
+print("5 / 2 =", 5/2)
+print("5 * 2 =", 5*2)
+print("5 ** 2 =", 5**2)
+print("5 % 2 =", 5%2)
+print("5 // 2 =", 5//2)
+
+#Order of operations is BODMAS, so put braces everywhere!
+
+#Ignoring Characters
+quote1 = "\"A Lannister always pays"
+quote2 = " his debt\""
+print(quote1 + quote2)
+
+#Multi line quotes
+multi_line_quote = '''A Lion does not concern himself
+with the opinion of sheep
+'''
+print(multi_line_quote)
+
+#Passing strings to print
+print("%s %s %s" %('I like the quotes', quote1+quote2, multi_line_quote))
