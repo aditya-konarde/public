c720c78c4f647958bcd853abe46a46f527840aa0
aditya <aditya@localhost.localdomain>
2016-08-26 20:55:40 +0530

    Added Easy setup script

---
+++ bash/setup.sh
@@ -0,0 +1,22 @@
+!/bin/bash
+
+#A shell script to make your life easy,by installing commonly used software
+#Suitable for a fresh installation of fedora 24
+#Author: Aditya Konarde
+#Date: 26 Aug 2016
+
+#First, Update all packages to their latest available versions
+echo 'Updating your system';
+dnf update -y;
+echo 'System Updated successfully';
+
+#Install some essentials
+echo 'Installing some good stuff';
+
+#Install RPMFusion Repos
+echo 'Installing RPMFusion Repos';
+su -c 'dnf install -y http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm vlc gnome-tweak-tool';
+
+#Install Fedy
+echo 'Installing Fedy';
+bash -c 'su -c "curl http://folkswithhats.org/fedy-installer -o fedy-installer && chmod +x fedy-installer && ./fedy-installer"'
