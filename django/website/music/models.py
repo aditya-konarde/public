afa059d85039846392c9fa26f67b0e4ed5c35281
aditya <aditya@heaven.example.com>
2016-12-25 16:23:14 +0530

    added some models, admin and stuff

--- django/website/music/models.py
+++ django/website/music/models.py
@@ -5,10 +5,14 @@ from django.db import models
 # Create your models here.
 class Album(models.Model):
     artist = models.CharField(max_length=250)
-    album_title = model.CharField(max_length=500)
-    genre = model.CharField(max_length=100)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=100)
 
-class Song():
+    def __str__(self):
+        return self.album_title + ' ' + self.artist
+
+class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
